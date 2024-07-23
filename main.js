aefdd0cb6753aa007d5a3d4580cfce0cd646884e
Ari O <arijoshry@gmail.com>
2024-07-22 20:25:00 -0700

    Big update 6-22

--- main.js
+++ main.js
@@ -1,18 +1,95 @@
-import { urls1 } from './urls1.js';
-import { news } from './news.js';
+import { national_news, science_and_tech, forign_news, extra, city, cable, finance, sports, fun, pop, long, nonprofit, polling_factcheck } from './urls1.js';
 
-function openRandomURLInNewTab() {
-    const dex = Math.floor(Math.random() * urls1.length);
-    const randomUrl = urls1[dex];
+const allURL = [
+    ...national_news,
+    ...science_and_tech,
+    ...forign_news,
+    ...extra,
+    ...city,
+    ...cable,
+    ...finance,
+    ...sports,
+    ...fun,
+    ...pop,
+    ...nonprofit,
+    ...polling_factcheck,
+    ...long
+];
+
+function openRandomURL(urlArray) {
+    const randomIndex = Math.floor(Math.random() * urlArray.length);
+    const randomUrl = urlArray[randomIndex];
     window.open(randomUrl, '_blank');
 }
 
-function openNews() {
-    const randomIndex = Math.floor(Math.random() * news.length);
-    const randomNews = news[randomIndex];
-    window.open(randomNews, '_blank');
+function openRandomURLInNewTab() {
+    openRandomURL(allURL);
+}
+
+function open_national_news() {
+    openRandomURL(national_news);
+}
+
+function open_science_and_tech() {
+    openRandomURL(science_and_tech);
+}
+
+function open_forign_news() {
+    openRandomURL(forign_news);
+}
+
+function open_extra() {
+    openRandomURL(extra);
+}
+
+function open_city() {
+    openRandomURL(city);
+}
+
+function open_cable() {
+    openRandomURL(cable);
+}
+
+function open_finance() {
+    openRandomURL(finance);
+}
+
+function open_sports() {
+    openRandomURL(sports);
+}
+
+function open_fun() {
+    openRandomURL(fun);
+}
+
+function open_pop() {
+    openRandomURL(pop);
+}
+
+function open_nonprofit() {
+    openRandomURL(nonprofit);
+}
+
+function open_polling_factcheck() {
+    openRandomURL(polling_factcheck);
+}
+
+function open_long() {
+    openRandomURL(long);
 }
 
 // Assign each function to a unique property on the window object
 window.openRandomURLInNewTab = openRandomURLInNewTab;
-window.openNews = openNews;
+window.open_national_news = open_national_news;
+window.open_science_and_tech = open_science_and_tech;
+window.open_forign_news = open_forign_news;
+window.open_extra = open_extra;
+window.open_city = open_city;
+window.open_cable = open_cable;
+window.open_finance = open_finance;
+window.open_sports = open_sports;
+window.open_fun = open_fun;
+window.open_pop = open_pop;
+window.open_nonprofit = open_nonprofit;
+window.open_polling_factcheck = open_polling_factcheck;
+window.open_long = open_long;
--- news.js
+++
@@ -1,6 +0,0 @@
-export const news = [
-    "https://theguardian.com",
-    "https://washingtonpost.com",
-    "https://nytimes.com",
-    "https://wsj.com"
-];
